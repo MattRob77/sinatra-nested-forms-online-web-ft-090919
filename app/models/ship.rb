8bac06817d2c2a317a9755358058b683596e14e5
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-22 19:06:41 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,19 @@
 class Ship
-  def 
+    attr_accessor :name, :type, :booty
+    @@ships = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@ships<< self
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships.clear
+    end
 end
