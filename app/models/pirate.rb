e16d971b522b60435fba2ac23648981c78aef7c3
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-22 19:10:19 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,19 +1,19 @@
-class Pirate 
-  attr_accessor :name, :weight, :height
-  
-  def initialize(args)
-    @name = args[:name]
-    @weight = args[:weight]
-    @height = args[:height]
-    @@all << self 
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def self.clear 
-    @@all.clear 
-  end 
-  
+class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
